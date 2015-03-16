fb5440af63cb0ce2fc5a02b7ad7a0f3b79b8e34f
ObjectMethod <richardpaulguy@gmail.com>
2015-03-16 13:24:53 -0400

    just to prove it crashes also, added ability to use WebImageManager

--- SDWebImageCrashTest/SDWebImageCrashTest/ViewController.m
+++ SDWebImageCrashTest/SDWebImageCrashTest/ViewController.m
@@ -12,9 +12,28 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
+    BOOL shouldUseCategory = YES;
+    
+    if (shouldUseCategory) {
+        [self useImageViewCategory];
+    } else {
+        [self useWebImageManager];
+    }
+}
+
+- (void) useImageViewCategory {
     NSString *urlString = @"https://s-passets-cache-ak0.pinimg.com/images/about/logos/Pinterest_Favicon.png";
     NSURL *url = [NSURL URLWithString:urlString];
     [self.imageView sd_setImageWithURL:url];
+}
+
+- (void) useWebImageManager {
+    NSString *urlString = @"https://s-passets-cache-ak0.pinimg.com/images/about/logos/Pinterest_Favicon.png";
+    NSURL *url = [NSURL URLWithString:urlString];
+    SDWebImageManager *webImageManager = [SDWebImageManager sharedManager];
+    [webImageManager downloadImageWithURL:url options:0 progress:nil completed:^(UIImage *image, NSError *error, SDImageCacheType cacheType, BOOL finished, NSURL *imageURL) {
+        
+    }];
 }
 
 @end
